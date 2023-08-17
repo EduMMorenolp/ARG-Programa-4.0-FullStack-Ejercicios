1afd117f071278961431b5ff5f157fb7a12922ae
Edu Moreno <e.m.morenolp@gmail.com>
2023-08-17 18:19:33 -0300

    Porfolio con React

---
+++ Guia-19 - React/Porfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Inicio from './componentes/Inicio';
+
+function App() {
+  return (
+    <div className="App">
+      <p> Desde App</p>
+      <Inicio></Inicio>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ Guia-19 - React/Porfolio/src/componentes/Inicio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Inicio() {
+  return (
+    <div className="App">
+      <h1> Hola soy Eduardo Moreno </h1>
+    </div>
+  );
+}
+
+export default Inicio;
