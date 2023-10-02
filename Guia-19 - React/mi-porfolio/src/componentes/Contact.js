1f5cfa3c6846acb0dad2be4249293570dc68ded5
Edu Moreno <e.m.morenolp@gmail.com>
2023-10-02 17:03:50 -0300

    add

--- Guia-19 - React/mi-porfolio/src/componentes/Contact.js
+++ Guia-19 - React/mi-porfolio/src/componentes/Contact.js
@@ -3,16 +3,29 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact" className="container text-center">
-      <h2 className="display-4">Contacto</h2>
-      <p className="lead">¡Puedes contactarme a través de los siguientes medios:</p>
-      <ul className="list-unstyled">
-        <li>Email: tuemail@example.com</li>
-        <li>LinkedIn: <a href="https://www.linkedin.com/in/tunombre">Tu Perfil de LinkedIn</a></li>
-        {/* Agrega más detalles de contacto según tus preferencias */}
-      </ul>
+      <div id="contacto" className="tarjeta">
+        <h2>Contacto</h2>
+        <p><a href="mailto:e.m.morenolp@gmail.com">e.m.morenolp@gmail.com</a></p>
+        <p>Curriculum Vitae: <a href="descargas/Curriculum%20Developer%202023.docx" download className="boton-descargar">Descargar CV</a></p>
+        <p>Si deseas ponerte en contacto conmigo o tienes alguna consulta, no dudes en enviarme un mensaje. Estaré encantado de ayudarte.</p>
+        <p>Puedes utilizar el formulario de contacto a continuación o escribirme directamente a mi dirección de correo electrónico.</p>
+        <p>¡Espero tener noticias tuyas pronto!</p>
+        <div className="social2-links">
+          <ul className="sidebar2">
+            <li><a href="https://www.linkedin.com/in/eduardo-m-moreno-programador/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin" style={{ color: '#0077B5' }}> LinkedIn</i></a></li>
+            <li><a href="https://github.com/EduMMorenolp" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> GitHub</i></a></li>
+            <li><a href="https://www.instagram.com/edu.m.morenolp/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram" style={{ color: '#E4405F' }}> Instagram</i></a></li>
+            <li><a href="https://twitter.com/EduMMorenolp" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter" style={{ color: '#1DA1F2' }}> Twitter</i></a></li>
+            <li><a href="https://discord.gg/HPdMbjPx" target="_blank" rel="noopener noreferrer"><i className="fab fa-discord" style={{ color: '#7289DA' }}> Discord</i></a></li>
+            <li><a href="https://www.youtube.com/@edummorenolp" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube" style={{ color: '#FF0000' }}> Youtube</i></a></li>
+            <li><a href="https://www.twitch.tv/edummorenolp" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitch" style={{ color: '#9146FF' }}> Twitch</i></a></li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
 
 export default Contact;
+
 
