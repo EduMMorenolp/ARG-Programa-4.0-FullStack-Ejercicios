bfb2c63bb1e7f986c451215f7d5a7860e8997873
Edu Moreno <e.m.morenolp@gmail.com>
2023-10-10 01:03:40 -0300

    oh shit here we go again

---
+++ Guia-20 - SPRING MVC/EjemploGuia/src/main/java/com/examplospring/EjemploGuia/EjemploGuiaApplication.java
@@ -0,0 +1,13 @@
+package com.examplospring.EjemploGuia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploGuiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploGuiaApplication.class, args);
+	}
+
+}
